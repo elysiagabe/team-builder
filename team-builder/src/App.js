2aff106074c5eca98b87f3f6456bddaff50dee36
Elysia Gabe <elysiagabe@gmail.com>
2020-02-26 12:16:20 -0800

    Renders initial team member data

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       Goodbye
-      <TeamMembers />
+      <TeamMembers teamMembers={teamMembers} />
       <TeamForm />
     </div>
   );
--- team-builder/src/Components/TeamMembers.js
+++ team-builder/src/Components/TeamMembers.js
@@ -1,8 +1,17 @@
 import React from "react";
 
-const TeamMembers = () => {
+const TeamMembers = ({teamMembers}) => {
+    console.log(teamMembers);
     return (
-        <div>Team Members</div>
+        <div>
+            {teamMembers.map(teamMember => (
+                <div key={teamMember.id}>
+                    <h2>{teamMember.name}</h2>
+                    <h3><strong>Role:</strong> {teamMember.role}</h3>
+                    <p>Say hello: {teamMember.email}</p>
+                </div>
+            ))}
+        </div>
     )
 }
 
