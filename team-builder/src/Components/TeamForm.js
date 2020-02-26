5ee1bac3429c48c43ac41a4b6a2c92ae863bd9a2
Elysia Gabe <elysiagabe@gmail.com>
2020-02-26 12:32:55 -0800

    Creates basic form structure

--- team-builder/src/Components/TeamForm.js
+++ team-builder/src/Components/TeamForm.js
@@ -2,7 +2,18 @@ import React from "react";
 
 const TeamForm = () => {
     return (
-        <div>Team Form</div>
+        <div>
+            <h2>Add a New Team Member</h2>
+            <form>
+                <label htmlFor="name">Name</label>
+                <input type="text" id="name" name="name" value="firstname lastname" />
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" value="email@gmail.com" />
+                <label htmlFor="role">Role</label>
+                <input type="text" id="role" name="role" value="job title" />
+                <button type="submit">Add to Team ></button>
+            </form>
+        </div>
     );
 }
 
--- team-builder/src/Components/TeamMembers.js
+++ team-builder/src/Components/TeamMembers.js
@@ -4,10 +4,11 @@ const TeamMembers = ({teamMembers}) => {
     console.log(teamMembers);
     return (
         <div>
+            <h2>Meet the Team</h2>
             {teamMembers.map(teamMember => (
                 <div key={teamMember.id}>
-                    <h2>{teamMember.name}</h2>
-                    <h3><strong>Role:</strong> {teamMember.role}</h3>
+                    <h3>{teamMember.name}</h3>
+                    <h4><strong>Role:</strong> {teamMember.role}</h4>
                     <p>Say hello: {teamMember.email}</p>
                 </div>
             ))}
